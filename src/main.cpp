26ea43ecc10254df0138015bb9543985dbb79090
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-12 03:38:55 +0100

    [guess_the_number] Finish the game

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,31 @@ int get_int_from_user()
     return n;
 }
 
+void play_guess_the_number()
+{
+    // Pick a random number
+    static constexpr int MIN             = 0;   // `static constexpr` is the "proper" way of declaring constants known at compile time
+    static constexpr int MAX             = 100; // It is as efficient as `#define` but has the benefit of working like a normal C++ variable: it has a type, etc.
+    const int            number_to_guess = rand(MIN, MAX);
+    std::cout << "I picked a number between " << MIN << " and " << MAX << '\n';
+    // Ask the user for a guess
+    bool finished = false;
+    while (!finished) {
+        const int user_guess = get_int_from_user();
+        if (user_guess < number_to_guess) {
+            std::cout << "Greater\n";
+        }
+        else if (user_guess > number_to_guess) {
+            std::cout << "Smaller\n";
+        }
+        else {
+            std::cout << "Congrats, you won!\n";
+            finished = true;
+        }
+    }
+}
+
 int main()
 {
-    const int n = get_int_from_user();
-    std::cout << "Got " << n << '\n';
+    play_guess_the_number();
 }
