1f853409ad2bd1f71d198f394f5a9d4da2616e67
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-18 23:42:55 +0100

    [hangman] Add player_has_won()

--- src/main.cpp
+++ src/main.cpp
@@ -25,11 +25,16 @@ bool player_is_alive(int number_of_lives)
     return number_of_lives > 0;
 }
 
+bool player_has_won(const std::vector<bool>& letters_guessed)
+{
+    return std::all_of(letters_guessed.begin(), letters_guessed.end(), [](bool letter_guessed) {
+        return letter_guessed;
+    });
+}
+
 int main()
 {
-    std::cout << player_is_alive(5) << '\n';
-    std::cout << player_is_alive(1) << '\n';
-    std::cout << player_is_alive(0) << '\n';
-    std::cout << player_is_alive(-1) << '\n';
-    std::cout << player_is_alive(-5) << '\n';
+    std::cout << player_has_won({true, false}) << '\n';
+    std::cout << player_has_won({true, true, true}) << '\n';
+    std::cout << player_has_won({false, true, true}) << '\n';
 }
