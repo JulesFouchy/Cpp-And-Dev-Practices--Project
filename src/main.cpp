2dbe98c7c49f38a9af013a5b17df8e6079f6c78a
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-22 03:51:36 +0100

    Add a menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
+#include "get_input_from_user.h"
 #include "hangman.h"
 #include "play_guess_the_number.h"
 
+void show_the_list_of_commands()
+{
+    std::cout << "What do you want to do?\n"
+                 "1: Play \"Guess the Number\"\n"
+                 "2: Play \"Hangman\"\n"
+                 "q: Quit\n";
+}
+
 int main()
 {
-    play_hangman();
+    bool quit = false;
+    while (!quit) {
+        show_the_list_of_commands();
+        const auto command = get_input_from_user<char>();
+        switch (command) {
+        case '1':
+            play_guess_the_number();
+            break;
+        case '2':
+            play_hangman();
+            break;
+        case 'q':
+            quit = true;
+            break;
+        default:
+            std::cout << "Sorry I don't know that command!\n";
+            break;
+        }
+    }
 }
