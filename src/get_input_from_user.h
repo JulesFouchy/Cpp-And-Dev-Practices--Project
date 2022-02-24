3f3539e5be9ac2ed0cf4cc4d5b8ffbb165f16fd9
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-24 11:06:29 +0100

    [get_input_from_user] Fix: replace sync() with ignore()
    Because sync() is not guaranteed to work on every platform, so it is basically useless :'(

--- src/get_input_from_user.h
+++ src/get_input_from_user.h
@@ -7,8 +7,8 @@ T get_input_from_user()
 {
     T input; // NOLINT
     while (!(std::cin >> input)) {
-        std::cin.clear(); // The user did not input a valid T so we need
-        std::cin.sync();  // to clear cin and try again
+        std::cin.clear();                                                   // The user did not input a valid T so we need
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // to clear cin and try again
         std::cout << "Invalid input, try again!\n";
     }
     return input;
