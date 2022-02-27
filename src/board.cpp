d00f025996abae103696745dab4db96bb3776b5f
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-27 18:37:32 +0100

    [board] Start using BoardSize instead of int

--- src/board.cpp
+++ src/board.cpp
@@ -1,34 +1,34 @@
 #include "board.h"
 
-float cell_radius(int board_size)
+float cell_radius(BoardSize board_size)
 {
-    return 1.f / static_cast<float>(board_size);
+    return 1.f / static_cast<float>(board_size.width);
 }
 
-glm::vec2 cell_bottom_left_corner(CellIndex index, int board_size)
+glm::vec2 cell_bottom_left_corner(CellIndex index, BoardSize board_size)
 {
     const auto idx = glm::vec2{static_cast<float>(index.x),
                                static_cast<float>(index.y)};
     return p6::map(idx,
-                   glm::vec2{0.f}, glm::vec2{static_cast<float>(board_size)},
+                   glm::vec2{0.f}, glm::vec2{static_cast<float>(board_size.width)},
                    glm::vec2{-1.f}, glm::vec2{1.f});
 }
 
-glm::vec2 cell_center(CellIndex index, int board_size)
+glm::vec2 cell_center(CellIndex index, BoardSize board_size)
 {
     return cell_bottom_left_corner(index, board_size) + cell_radius(board_size);
 }
 
-void draw_cell(CellIndex index, int board_size, p6::Context& ctx)
+void draw_cell(CellIndex index, BoardSize board_size, p6::Context& ctx)
 {
     ctx.square(p6::BottomLeftCorner{cell_bottom_left_corner(index, board_size)},
                p6::Radius{cell_radius(board_size)});
 }
 
-void draw_board(int size, p6::Context& ctx)
+void draw_board(BoardSize size, p6::Context& ctx)
 {
-    for (int x = 0; x < size; ++x) {
-        for (int y = 0; y < size; ++y) {
+    for (int x = 0; x < size.width; ++x) {
+        for (int y = 0; y < size.height; ++y) {
             draw_cell({x, y}, size, ctx);
         }
     }
--- src/board.h
+++ src/board.h
@@ -7,17 +7,32 @@ struct CellIndex {
     int y;
 };
 
-float cell_radius(int board_size);
+struct BoardSize {
+    int width;
+    int height;
 
-glm::vec2 cell_bottom_left_corner(CellIndex index, int board_size);
+    BoardSize(int size)
+        : width{size}, height{size}
+    {
+    }
 
-glm::vec2 cell_center(CellIndex index, int board_size);
+    BoardSize(int width, int height)
+        : width{width}, height{height}
+    {
+    }
+};
+
+float cell_radius(BoardSize board_size);
+
+glm::vec2 cell_bottom_left_corner(CellIndex index, BoardSize board_size);
+
+glm::vec2 cell_center(CellIndex index, BoardSize board_size);
 
 /// Draws a cell at the position specified by `index`
 /// It uses the current context's fill, stroke and stroke_weight
-void draw_cell(CellIndex index, int board_size, p6::Context& ctx);
+void draw_cell(CellIndex index, BoardSize board_size, p6::Context& ctx);
 
 /// Draws a game board
 /// size is the number of rows and the number of columns
 /// It uses the current context's fill, stroke and stroke_weight
-void draw_board(int size, p6::Context& ctx);
+void draw_board(BoardSize size, p6::Context& ctx);
