c01ecf6bb1012eff583b28bf4ae5115d8700e334
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-27 19:54:40 +0100

    [connect_4] Add try_to_play_in_column()

--- src/connect_4.cpp
+++ src/connect_4.cpp
@@ -33,13 +33,38 @@ void draw_tokens(const Board& board, p6::Context& ctx)
     }
 }
 
+std::optional<int> try_to_find_lowest_empty_row_index(int column_index, const Board& board)
+{
+    for (int y = 0; y < board.height(); ++y) {
+        if (!board[{column_index, y}].has_value()) {
+            return std::make_optional(y);
+        }
+    }
+    return std::nullopt;
+}
+
+/// Returns true iff the column was not full and a token has been successfully added to the column.
+bool try_to_play_in_column(int column_index, Player player, Board& board)
+{
+    const auto row_index = try_to_find_lowest_empty_row_index(column_index, board);
+    if (row_index.has_value()) {
+        board[{column_index, *row_index}] = std::make_optional(player);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 void play_connect_4()
 {
-    auto ctx      = p6::Context{{1200, 800, "Connect 4"}};
-    auto board    = Board{};
-    board[{0, 3}] = std::make_optional(Player::Red);
-    board[{1, 2}] = std::make_optional(Player::Yellow);
-    ctx.update    = [&]() {
+    auto ctx   = p6::Context{{1200, 800, "Connect 4"}};
+    auto board = Board{};
+    try_to_play_in_column(0, Player::Red, board);
+    try_to_play_in_column(1, Player::Yellow, board);
+    try_to_play_in_column(0, Player::Red, board);
+    try_to_play_in_column(0, Player::Yellow, board);
+    ctx.update = [&]() {
         ctx.background({.3f, 0.25f, 0.35f});
         ctx.fill = {1.f, 1.f, 1.f, 0.5f};
         draw_board(board.size(), ctx);
